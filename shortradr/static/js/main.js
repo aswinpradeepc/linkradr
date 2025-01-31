bf2fa61c665efa3b1214708f89b66f6455b830d7
aswinpradeepc <aswinpradeepc@gmail.com>
2025-01-31 23:19:11 +0530

    css fixed -not optimal

--- shortradr/static/js/main.js
+++ shortradr/static/js/main.js
@@ -1,12 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.querySelector('.hamburger-menu');
     const mobileNavOverlay = document.querySelector('.mobile-nav-overlay');
+    const closeNav = document.querySelector('.close-nav'); // Select close button
     const mobileNavLinks = document.querySelectorAll('.mobile-nav-link');
 
-    if (hamburger && mobileNavOverlay) {
+    if (hamburger && mobileNavOverlay && closeNav) {
         hamburger.addEventListener('click', () => {
             mobileNavOverlay.classList.toggle('active');
             hamburger.classList.toggle('active');
+        });
+
+        closeNav.addEventListener('click', () => {  // Handle close button click
+            mobileNavOverlay.classList.remove('active');
+            hamburger.classList.remove('active');
         });
 
         mobileNavLinks.forEach(link => {
