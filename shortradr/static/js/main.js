b5d14fd7bf031176714afdef72c667c6d5d25ed7
aswinpradeepc <aswinpradeepc@gmail.com>
2025-01-31 17:02:09 +0530

    bearable ui

--- shortradr/static/js/main.js
+++ shortradr/static/js/main.js
@@ -3,15 +3,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const mobileNavOverlay = document.querySelector('.mobile-nav-overlay');
     const mobileNavLinks = document.querySelectorAll('.mobile-nav-link');
 
-    hamburger.addEventListener('click', () => {
-        mobileNavOverlay.classList.toggle('active');
-        hamburger.classList.toggle('active');
-    });
-
-    mobileNavLinks.forEach(link => {
-        link.addEventListener('click', () => {
-            mobileNavOverlay.classList.remove('active');
-            hamburger.classList.remove('active');
+    if (hamburger && mobileNavOverlay) {
+        hamburger.addEventListener('click', () => {
+            mobileNavOverlay.classList.toggle('active');
+            hamburger.classList.toggle('active');
         });
-    });
+
+        mobileNavLinks.forEach(link => {
+            link.addEventListener('click', () => {
+                mobileNavOverlay.classList.remove('active');
+                hamburger.classList.remove('active');
+            });
+        });
+    }
 });
