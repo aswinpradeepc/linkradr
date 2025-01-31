b379797b219794821ae7b9f885708223cb6bd3de
aswinpradeepc <aswinpradeepc@gmail.com>
2025-02-01 04:28:30 +0530

    version0 complete

--- shortradr/shortener/views.py
+++ shortradr/shortener/views.py
@@ -1,22 +1,19 @@
-from django.db import IntegrityError
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import URLShortener
 from django.contrib.auth.models import User
-from django.http import HttpResponse, JsonResponse
 import json
-
 
 def home(request):
     return render(request, 'shortener/home.html')
 
 @login_required
 def profile(request):
-    user_urls = URLShortener.objects.filter(user=request.user)
+    user_urls = URLShortener.objects.filter(user=request.user).order_by('-id')
     fname = request.user.first_name
-    return render(request, 'shortener/profile.html', { 'fname': fname ,'user_urls': user_urls})
-
+    latest_url_id = request.GET.get('latest_url_id')
+    return render(request, 'shortener/profile.html', { 'fname': fname, 'user_urls': user_urls, 'latest_url_id': latest_url_id })
 
 @login_required
 def shorten_url(request):
@@ -24,26 +21,18 @@ def shorten_url(request):
         original_url = request.POST.get('url')
         if original_url:
             try:
-                # Try to create a new shortened URL
                 shortened_obj = URLShortener.objects.create(
                     actual_url=original_url, 
                     user=request.user
                 )
-                short_url = f"{request.scheme}://{request.get_host()}/{shortened_obj.shortened_url}"
-                return HttpResponse(f"Shortened URL: {short_url}")
-            
+                return redirect(f'/profile/?latest_url_id={shortened_obj.id}')
             except IntegrityError:
-                # If URL already exists for this user, retrieve the existing one
                 existing_url = URLShortener.objects.get(
                     actual_url=original_url, 
                     user=request.user
                 )
-                short_url = f"{request.scheme}://{request.get_host()}/{existing_url.shortened_url}"
-                return HttpResponse(f"URL already exists. Existing shortened URL: {short_url}")
-    
+                return redirect(f'/profile/?latest_url_id={existing_url.id}')
     return HttpResponse("Invalid request", status=400)
-
-
 
 def redirect_url(request, short_code):
     url_obj = get_object_or_404(URLShortener, shortened_url=short_code)
